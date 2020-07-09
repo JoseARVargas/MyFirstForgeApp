c649d4816006b32b39c851d394bd9b228e2842bd
Jose Vargas <jaribeirovargas@gmail.com>
2020-07-08 21:38:52 -0300

    Add project files.

---
+++ MyFirstForgeApp/Controllers/OSSController.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Forge;
+using Autodesk.Forge.Model;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace MyFirstForgeApp.Controllers
+{
+    public class OSSController : ApiController
+    {
+        public string ClientId { get { return OAuthController.GetAppSetting("FORGE_CLIENT_ID").ToLower(); } }
+
+        /// <summary>
+        /// Return list of buckets (id=#) or list of objects (id=bucketKey)
+        /// </summary>
+        [HttpGet]
+        [Route("api/forge/oss/bucket")]
+        public async Task<IList<TreeNode>> GetOSSAsync([FromUri]string id)
+        {
+            IList<TreeNode> nodes = new List<TreeNode>();
+            dynamic oauth = await OAuthController.GetInternalAsync();
+
+            if (id == "#") // root
+            {
+                // in this case, let's return all buckets
+                BucketsApi appBckets = new BucketsApi();
+                appBckets.Configuration.AccessToken = oauth.access_token;
+
+                // to simplify, let´s return only the first 100 buckets
+                dynamic buckets = await appBckets.GetBucketAsync("US", 100);
+                foreach (KeyValuePair<string, dynamic> bucket in new DynamicDictionaryItems(buckets.items))
+                {
+                    nodes.Add(new TreeNode(bucket.Value.bucketKey, bucket.Value.bucketKey.Replace(ClientId + "-", string.Empty), "bucket", true));
+                }
+            }
+            else
+            {
+                // as we have the id (bucketKey), let´s return all
+                ObjectsApi objects = new ObjectsApi();
+                objects.Configuration.AccessToken = oauth.access_token;
+            }
+        }
+
+    }
+}
